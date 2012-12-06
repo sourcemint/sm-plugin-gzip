7511ec0f933af9afe9f037faced945f187c8ca4b
cadorn <christoph@christophdorn.com>
2012-12-06 15:27:35 -0800

    initial implementation

---
+++ plugin.js
@@ -0,0 +1,35 @@
+
+const FS = require("fs");
+const ZLIB = require("zlib");
+const TAR = require("tar");
+
+
+exports.for = function(API, plugin) {
+
+    plugin.extract = function(fromPath, toPath, options) {
+        var deferred = API.Q.defer();
+        // TODO: Use os command if available as it is much faster.
+        var stream = FS.createReadStream(fromPath);
+        stream.on("error", function(err) {
+            deferred.reject(err);
+        });
+        var unzipper = ZLIB.createGunzip();
+        unzipper.on("error", function(err) {
+            deferred.reject(err);
+        });
+        var extracter = TAR.Extract({
+            path: toPath,
+            strip: 1
+        });
+        extracter.on("error", function(err) {
+            deferred.reject(err);
+        });
+        extracter.on("end", function() {
+            deferred.resolve(200);
+        });
+        stream.pipe(unzipper).pipe(extracter);
+        return deferred.promise;
+    };
+
+}
+
